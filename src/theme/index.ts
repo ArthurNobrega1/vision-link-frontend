5d0735e629d836034066676cd059ba9779046add
ArthurNobrega1 <arthurnobrega906@gmail.com>
2024-11-07 08:40:29 -0300

    create register and add api connection

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -7,11 +7,13 @@ const theme = {
         SECONDARY_300: '#A59782',
         SECONDARY_200: '#C9BDAB',
         SECONDARY_100: '#D9D9D9',
+        PLACEHOLDER: '#C9BDAB80'
     },
     FONT_FAMILY: {
         REGULAR: 'DMSans_400Regular'
     },
     FONT_SIZE: {
+        XS: 14,
         SM: 16,
         MD: 20,
         LG: 24,
